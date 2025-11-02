caf0a3b1d89c8fad6bd08616136971ea39211240
Bryan Ramirez <101440787+BryanRamirez21@users.noreply.github.com>
2025-11-02 14:32:42 -0600

    Delete app directory

--- app/views/jefe_area/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('BASE_PATH')) define('BASE_PATH','/sistema_rh');
-$titulo_pagina = "Inicio · Jefe de Área";
-require_once $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/app/views/shared/header.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/app/views/shared/home_feed.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/app/views/shared/footer.php';
